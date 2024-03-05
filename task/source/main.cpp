eaec87c3df92f1c13752225d9ba4a7ca6a56e99d
Piotr Maszczak <piotrmaszczak25@gmail.com>
2024-03-05 15:24:46 +0100

    Add initial files

---
+++ task/source/main.cpp
@@ -0,0 +1,9 @@
+int main(int argc, char** argv)
+{
+    // TODO: 1. Prepare example data for serialization
+    // TODO: 2. Serialize data to file
+    // TODO: 3. Deserialize data from file
+    // TODO: 4. Verify deserialized data is correct
+
+    return 0;
+}
